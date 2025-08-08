f83cfa6bedb16ad9606c2fae460a723424e18895
marouane-sadoune <marouanesadoune@gmail.com>
2025-08-08 15:16:48 +0100

    logc of payroll

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\Rules\Can;
 
 class Contract extends Model
 {
@@ -21,8 +22,19 @@ class Contract extends Model
             $query->inCompany();
         });
     }
-    public function getDuration()
+    public function getDurationAttribute()
     {
         return Carbon::parse($this->start_date)->diffForHumains($this->end_date);
     }
+    public function getSearchByEmployee($query, $name)
+    {
+        return $query->whereHas('employee', function ($query) use ($name) {
+            $query->where('name', 'like', '%' . $name . '%');
+        });
+    }
+    public function getTotaleEarning($monthYear)
+    {
+        return $this-> rate_type = 'monthly' ? $this->rate : $this->rate * Carbon::parse($monthYear)->daysInMonth;
+    }
+    
 }
--- app/Models/Payroll.php
+++ app/Models/Payroll.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Payroll extends Model
 {
-    //
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }public function Salaries()
+    {
+        return $this->hasMany(Salary::class);
+    }
+    public function paiments()
+    {
+        return $this->hasMany(Payment::class);
+    }
+    public function scopeInCompany($query, $companyId)
+    {
+        return $query->where('company_id', $this->company_id);
+
+    }
+    public function getMonthYearAttribute()
+    {
+        return $this->$this->year . ' ' . $this->month;
+    }
+    public function getMonthStringAttribute()
+    {
+        return Carbon::createFromDate($this->month_year)->format('F Y');
+    }
 }
