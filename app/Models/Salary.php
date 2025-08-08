40519d2263dc30fb0c98d360b1f5a9be65c16e40
marouane-sadoune <marouanesadoune@gmail.com>
2025-08-08 22:58:59 +0100

    do salary

--- app/Models/Salary.php
+++ app/Models/Salary.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Salary extends Model
 {
-    //
+    protected $fillable = [
+        'employee_id',
+        'payroll_id',
+        'gross_salary',
+    ];
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+    public function payroll()
+    {
+        return $this->belongsTo(Payroll::class);
+    }
 }
