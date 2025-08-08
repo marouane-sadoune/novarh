b287f9a344cb39c87742971fdf05f375f9744380
marouane-sadoune <marouanesadoune@gmail.com>
2025-08-08 23:36:13 +0100

    start routing

---
+++ app/Livewire/Admin/Designations/Edite.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Designations;
+
+use Livewire\Component;
+
+class Edite extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.designations.edite');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Admin;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
@@ -7,9 +8,15 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', Admin\Dashboard::class)->name('dashboard');
+    Route::prefix('company')->name('companies.')->group(function () {
+        Route::get('/', Admin\Companies\Index::class)->name('index');
+        Route::get('/create', Admin\Companies\Create::class)->name('create');
+        Route::get('/{id}/edit', Admin\Companies\Edit::class)->name
+    });
+    })->name('dashboard');
+
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
