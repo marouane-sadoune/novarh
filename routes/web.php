8fcd1f8c2716725f90acac133850368dc9409a5f
marouane-sadoune <marouanesadoune@gmail.com>
2025-08-10 00:33:43 +0100

    fix route

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,48 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::prefix('company')->name('companies.')->group(function () {
         Route::get('/', Admin\Companies\Index::class)->name('index');
         Route::get('/create', Admin\Companies\Create::class)->name('create');
-        Route::get('/{id}/edit', Admin\Companies\Edit::class)->name
-    });
-    })->name('dashboard');
+        Route::get('/{id}/edit', Admin\Companies\Edite::class)->name('edit');
+        });
+            // For Departments
+        Route::prefix('departement')->name('departements.')->group(function () {
+            Route::get('/', Admin\Departement\Index::class)->name('index');
+            Route::get('/create', Admin\Departement\Create::class)->name('create');
+            Route::get('/{id}/edit', Admin\Departement\Edite::class)->name('edit');
+        });
+
+        // For Designations
+        Route::prefix('designations')->name('designations.')->group(function () {
+            Route::get('/', Admin\Designations\Index::class)->name('index');
+            Route::get('/create', Admin\Designations\Create::class)->name('create');
+            Route::get('/{id}/edit', Admin\Designations\Edite::class)->name('edit');
+        });
+
+        // For Employees
+        Route::prefix('employees')->name('employees.')->group(function () {
+            Route::get('/', Admin\Employees\Index::class)->name('index');
+            Route::get('/create', Admin\Employees\Create::class)->name('create');
+            Route::get('/{id}/edit', Admin\Employees\Edite::class)->name('edit');
+        });
+
+        // For Contracts
+        Route::prefix('contracts')->name('contracts.')->group(function () {
+            Route::get('/', Admin\Contracts\Index::class)->name('index');
+            Route::get('/create', Admin\Contracts\Create::class)->name('create');
+            Route::get('/{id}/edit', Admin\Contracts\Edite::class)->name('edit');
+        });
+
+        // For Payrolls
+        Route::prefix('payrolls')->name('payrolls.')->group(function () {
+            Route::get('/', Admin\Payrolls\Index::class)->name('index');
+            Route::get('/{id}', Admin\Payrolls\Show::class)->name('show');
+        });
+
+        // For Payments
+        Route::prefix('payments')->name('payments.')->group(function () {
+            Route::get('/', Admin\Payments\Index::class)->name('index');
+            Route::get('/{id}', Admin\Payments\Show::class)->name('show');
+        });
+})->name('dashboard');
 
 
 Route::middleware(['auth'])->group(function () {
