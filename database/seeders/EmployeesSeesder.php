a48bb2a4b443bb6f703214e5de055fb92afe17db
marouane-sadoune <marouanesadoune@gmail.com>
2025-08-12 22:17:51 +0100

    using faker to generate random data

--- database/seeders/EmployeesSeesder.php
+++ database/seeders/EmployeesSeesder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
+use App\Models\Employee;
+use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,24 @@ class EmployeesSeesder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Factory::create();
+        $companies = Company::all();
+        foreach ($companies as $key => $company) {
+            foreach($company->departments as $department) {
+                foreach($department->designations as $key => $designation) {
+                    for ($i = 0; $i < 5; $i++) {
+                        Employee::create([
+                            'name' => $faker->name,
+                            'email' => $faker->unique()->safeEmail,
+                            'phone' => $faker->phoneNumber,
+                            'address' => $faker->address,
+                            // 'department_id' => $department->id,
+                            // 'designation_id' => $designation->id,
+                            // 'company_id' => $company->id,
+                        ]);
+                    }
+                }
+            }
+        }
     }
 }
