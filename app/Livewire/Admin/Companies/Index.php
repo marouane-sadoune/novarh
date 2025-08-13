25eae7529b58a2f39dc00a18aa8e6683b3beedf6
marouane-sadoune <marouanesadoune@gmail.com>
2025-08-13 22:55:17 +0100

    basic crud in companies

--- app/Livewire/Admin/Companies/Index.php
+++ app/Livewire/Admin/Companies/Index.php
@@ -2,10 +2,24 @@
 
 namespace App\Livewire\Admin\Companies;
 
+use App\Models\Company;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public function delete($companyId)
+    {
+        $company = Company::find($companyId);
+        if ($company->exists) {
+            $company->delete();
+            session()->flash('message', 'Company deleted successfully.');
+            if ($company->logo) {
+                $company->logo->delete();
+            }
+        } else {
+            session()->flash('error', 'Company not found.');
+        }
+    }
     public function render()
     {
         return view('livewire.admin.companies.index');
