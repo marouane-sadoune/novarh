b08320790aa9a8a1523a2acb119cc23d78def662
marouane-sadoune <marouanesadoune@gmail.com>
2025-08-13 23:12:25 +0100

    addCompanies

--- app/Livewire/Admin/Companies/Create.php
+++ app/Livewire/Admin/Companies/Create.php
@@ -6,6 +6,19 @@ use Livewire\Component;
 
 class Create extends Component
 {
+    use \Livewire\WithFileUploads;
+    public $company;
+    public $logo;
+
+    public function rules()
+    {
+        return [
+            'company.name' => 'required|string|max:255',
+            'company.website' => 'nullable|url|max:255',
+            'company.email' => 'required|email|max:255',
+            'logo' => 'nullable|image|mimes:jpg,png,jpeg|max:2048', // 2MB Max
+        ];
+    }
     public function render()
     {
         return view('livewire.admin.companies.create');
