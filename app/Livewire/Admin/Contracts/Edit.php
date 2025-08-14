77fee8c00ccb3cab5acf4645b87c111999fbf9fe
marouane-sadoune <marouanesadoune@gmail.com>
2025-08-14 19:39:05 +0100

    fixContract

--- app/Livewire/Admin/Contracts/Edit.php
+++ app/Livewire/Admin/Contracts/Edit.php
@@ -7,28 +7,7 @@ use Livewire\Component;
 
 class Edit extends Component
 {
-    public $department;
-
-    public function rules()
-    {
-        return [
-            'department.name' => 'required|string|max:255',
-        ];
-    } 
-    public function mount($id)
-    {
-        $this->department = Department::find($id);
-    }
-    public function save()
-    {
-        $this->validate();
-
-        $this->department->save();
-
-        session()->flash('success', 'Department edited successfully.');
-
-        return redirect()->route('admin.contracts.index');
-    }
+    
     public function render()
     {
         return view('livewire.admin.contracts.edite');
--- app/Livewire/Admin/Contracts/Index.php
+++ app/Livewire/Admin/Contracts/Index.php
@@ -9,13 +9,7 @@ use Livewire\WithPagination;
 
 class Index extends Component
 {
-    use WithPagination, WithoutUrlPagination;
-
-    public function delete($id)
-    {
-        Department::find($id)->delete();
-        session()->flash('success', 'Departement deleted successfully.');
-    }
+    
     public function render()
     {
         return view('livewire.admin.contracts.index',data: [
