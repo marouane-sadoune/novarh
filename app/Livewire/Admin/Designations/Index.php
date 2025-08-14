2b49be379d401dbd26805999f9f14263719ef4bf
marouane-sadoune <marouanesadoune@gmail.com>
2025-08-14 19:45:16 +0100

    IndexDesugnation

--- app/Livewire/Admin/Designations/Index.php
+++ app/Livewire/Admin/Designations/Index.php
@@ -2,10 +2,20 @@
 
 namespace App\Livewire\Admin\Designations;
 
+use App\Models\Designation;
 use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination, WithoutUrlPagination;
+
+    public function delete($id)
+    {
+        Designation::find($id)->delete();
+        session()->flash('success', 'Designation deleted successfully.');
+    }
     public function render()
     {
         return view('livewire.admin.designations.index');
