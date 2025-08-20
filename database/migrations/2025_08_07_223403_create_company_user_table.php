290c4d8528b0348e79c4e100bce356d4376733fa
marouane-sadoune <marouanesadoune@gmail.com>
2025-08-20 23:15:11 +0100

    aml

--- database/migrations/2025_08_07_223403_create_company_user_table.php
+++ database/migrations/2025_08_07_223403_create_company_user_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('company_user', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('company_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->primary(['company_id', 'user_id']);
